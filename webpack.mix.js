251d1a3e76af9cdeffd518e50be7836cbb567d39
Yoshitaka Shimazu <y.shimazu210@gmail.com>
2020-06-12 16:47:35 +0900

    vue_profileから移行

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,17 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/home.js', 'public/js')
+    .js('resources/js/skill.js', 'public/js')
+    .js('resources/js/actual_result.js', 'public/js')
+    .js('resources/js/development.js', 'public/js')
+    .js('resources/js/contact.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css', {
+      implementation: require('node-sass')
+    });
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.browserSync({
+  files: [
+    "public/**/*.*"
+  ],
+  proxy: "http://localhost:8000"
+});
